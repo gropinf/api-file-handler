f700765bdfda4ee93b333e732d13fe1105915f6e
gropinf <39649710+gropinf@users.noreply.github.com>
2024-12-03 14:59:58 -0300

    Ajustes Api WebHook Asaas

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const multer = require("multer");
 const { readFile } = require("./utils/readFile");
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid"); // Para gerar IDs únicos
 const { authMiddleware } = require("./utils/authMiddleware");
+//const db = require('./utils/db'); // Importa a conexão com o banco de dados
 
 const app = express();
 
@@ -18,6 +20,7 @@ const uploadFolder = path.join(__dirname, "uploads");
 if (!fs.existsSync(uploadFolder)) {
   fs.mkdirSync(uploadFolder, { recursive: true });
 }
+
 
 // Middleware global
 app.use(bodyParser.json({ limit: "10mb" }));
@@ -59,14 +62,15 @@ app.post("/upload", authMiddleware, (req, res) => {
 // 2) Rota de extração de texto de arquivos (base64)
 app.post("/extract-text", authMiddleware, async (req, res) => {
   const { base64, fileType } = req.body;
-
+  console.log(`fileType ${fileType}`);
+  console.log(`base64 ${base64}`);
   if (!base64 || !fileType) {
     return res.status(400).json({ error: "extract-text = Base64 e fileType são obrigatórios" });
 
   }
 
-  const buffer = Buffer.from(base64, "base64");
-  const filePath = `uploads/${Date.now()}`;
+  //const buffer = Buffer.from(base64, "base64");
+  //const filePath = `uploads/${Date.now()}`;
   fs.writeFileSync(filePath, buffer);
 
   try {
@@ -81,6 +85,50 @@ app.post("/extract-text", authMiddleware, async (req, res) => {
 app.get("/status", (req, res) => {
   res.json({ status: "API is running", port: PORT });
 });
+
+
+// Endpoint POST `/asaas_hook`
+app.post("/asaas_hook", async (req, res) => {
+  try {
+    const { id, event, dateCreated, payment } = req.body;
+
+    // Validar os dados recebidos
+    if (!id || !event || !dateCreated ) {
+      return res.status(400).json({ error: "Todos os campos são obrigatórios" });
+    }
+
+    // Configurar o payload para a API externa
+    const payload = {
+      Acoes: [
+        {
+          id_evento: id, // Usar o ID recebido
+          id_filial: "07984640000122", // Informação fixa
+          //id_evento: "asaas", // Informação fixa
+          tipo_evento: "payment", // Informação fixa
+          status: "0", // Informação fixa
+        },
+      ],
+    };
+
+    // Configurar os headers para a requisição
+    const headers = {
+      Authorization: process.env.TARGET_AUTH,
+      "Content-Type": "application/json",
+    };
+
+    // Enviar os dados para a API externa
+    const response = await axios.post(process.env.TARGET_URL, payload, { headers });
+
+    // Retornar a resposta da API externa ao cliente
+    res.status(response.status).json({ message: "Dados enviados com sucesso!", data: response.data });
+  } catch (error) {
+    console.error("Erro ao enviar dados para a API externa:", error.message);
+
+    // Retornar erro caso ocorra
+    res.status(500).json({ error: "Erro ao processar a requisição" });
+  }
+});
+
 
 // Inicia o servidor
 app.listen(PORT, () => {
